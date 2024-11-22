f140499611d4edae7a8e598c464fc8e578681c54
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2024-11-22 14:12:08 +0000

    Worked on date functions

--- days_api/date_functions.py
+++ days_api/date_functions.py
@@ -4,16 +4,24 @@ from datetime import datetime, date
 
 
 def convert_to_datetime(date_val: str) -> datetime:
-    pass
+    # `DD.MM.YYYY`
+    return datetime.strptime(date_val, "%d.%m.%Y")
 
 
 def get_days_between(first: datetime, last: datetime) -> int:
-    pass
+    return (last - first).days
+    
 
 
 def get_day_of_week_on(date_val: datetime) -> str:
-    pass
+    return convert_to_datetime(date_val).strftime("%A")
 
 
 def get_current_age(birthdate: date) -> int:
-    pass
+    print(date.now())
+
+# a = convert_to_datetime("11.01.2001")
+# b = convert_to_datetime("15.01.2001")
+# print(get_days_between(a, b))
+# print(get_day_of_week_on("21.11.2024"))
+get_current_age(datetime.now())
